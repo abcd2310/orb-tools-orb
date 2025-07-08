9926d8c3ccfb68f79dace6bcbf77b993ee315944
abcd2310 <federica2310govoni@gmail.com>
2025-07-08 04:18:20 +0200

    Delete src directory

--- src/scripts/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -d "$ORB_VAL_SOURCE_DIR" ]; then
-    printf "No source directory located at %s\n" "$ORB_VAL_SOURCE_DIR"
-    printf "This orb assumes you have built your orb using the Orb Development Kit\n"
-    printf "https://circleci.com/docs/2.0/orb-author/#orb-development-kit\n"
-    exit 1
-fi
-pip install --user yamllint
-yamllint "$ORB_VAL_SOURCE_DIR"
--- src/scripts/pack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ORB_DIR=${ORB_VAL_ORB_DIR%/}
-ORB_FILE=${ORB_VAL_ORB_FILE_NAME#/}
-
-mkdir -p "$ORB_VAL_ORB_DIR" &&
-  circleci orb pack --skip-update-check "$ORB_VAL_SOURCE_DIR" >"${ORB_DIR}/${ORB_FILE}"
